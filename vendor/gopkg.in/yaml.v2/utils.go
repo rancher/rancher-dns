0c83a9ef196ced4964e8ca37f4d9e4d05b925aaf
Darren Shepherd <darren@rancher.com>
2016-03-16 21:26:24 -0700

    Update build scripts

---
+++ vendor/gopkg.in/yaml.v2/utils.go
@@ -0,0 +1,25 @@
+package yaml
+
+func convertKeysToStrings(item interface{}) interface{} {
+	switch typedDatas := item.(type) {
+
+	case map[interface{}]interface{}:
+		newMap := make(map[string]interface{})
+
+		for key, value := range typedDatas {
+			stringKey := key.(string)
+			newMap[stringKey] = convertKeysToStrings(value)
+		}
+		return newMap
+
+	case []interface{}:
+		var newArray []interface{}
+		for _, value := range typedDatas {
+			newArray = append(newArray, convertKeysToStrings(value))
+		}
+		return newArray
+
+	default:
+		return item
+	}
+}
